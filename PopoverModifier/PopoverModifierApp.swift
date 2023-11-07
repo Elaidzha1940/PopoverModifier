bc50b8a74d63c216927a91a97b2bb27dd47cdfd3
Elaidzha1940 <eli080293@gmail.com>
2023-11-07 19:30:03 +0300

    PopoverModifier

---
+++ PopoverModifier/PopoverModifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopoverModifierApp.swift
+//  PopoverModifier
+//
+//  Created by Elaidzha Shchukin on 07.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct PopoverModifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
